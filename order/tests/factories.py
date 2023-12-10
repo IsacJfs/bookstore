4587388736b0b15f3805211455184cdc8d56f032
IsacJfs <isac.jfs@gmail.com>
2023-12-10 16:26:46 -0300

    :heavy_plus_sign: [build] criando serializers e implementando factories

---
+++ order/tests/factories.py
@@ -0,0 +1,27 @@
+import factory
+
+from django.contrib.auth.models import User
+from product.tests.factories import ProductFactory 
+from order.models import Order
+
+class UserFactory(factory.django.DjangoModelFactory):
+    username = factory.Faker('pystr')
+    email = factory.Faker('pystr')
+
+    class Meta:
+        model = User
+
+class OrderFactory(factory.django.DjangoModelFactory):
+    user = factory.LazyAttribute(UserFactory)
+    
+    @factory.post_generation
+    def products(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            for product in extracted:
+                self.products.add(product)
+
+    class Meta:
+        model = Order
