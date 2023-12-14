3d298258e71f2c0caee7da921fd892d73b90a7f9
IsacJfs <isac.jfs@gmail.com>
2023-12-14 11:55:54 -0300

    :heavy_plus_sign: [feat] adicionando templates e webhooks

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-load_dotenv("../.env")
+load_dotenv("/home/isacjfs/bookstore/.env")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -66,7 +66,7 @@ ROOT_URLCONF = "bookstore.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'bookstore', 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
